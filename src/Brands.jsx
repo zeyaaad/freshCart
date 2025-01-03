97c9c2569c359d53bbabfc458eb181f957f5f410
Zeyad Mohamed <114029186+zeyaaad@users.noreply.github.com>
2025-01-03 05:08:28 +0200

    Update Brands.jsx

--- src/Brands.jsx
+++ src/Brands.jsx
@@ -8,7 +8,7 @@ export default function Brands() {
 
   async function getbrands() {
     setloadin(true)
-    let {data}= await axios.get("https://route-ecommerce.onrender.com/api/v1/brands") 
+    let {data}= await axios.get('https://ecommerce.routemisr.com/api/v1/brands') 
     setbrands(data.data)
     setloadin(false)
   }
